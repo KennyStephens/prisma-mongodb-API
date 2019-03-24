71d2f9bef565b1e5d35cb2187775d68cdde7c8fe
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-24 12:14:53 -0600

    cant figure out how to read from correct database

--- overwatch-prisma/generated/prisma-client/index.js
+++ overwatch-prisma/generated/prisma-client/index.js
@@ -5,7 +5,7 @@ var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: "OverwatchPrisma",
+    name: "Owcharacter",
     embedded: false
   }
 ];
--- overwatch-prisma/index.js
+++ overwatch-prisma/index.js
@@ -6,16 +6,16 @@ async function main() {
   // // Create a new user called `Alice`
 
   // console.log(prisma);
-  const newUser = await prisma.createOverwatchPrisma({ name: 'kenny', class: 'support'})
+  // const newUser = await prisma.createOverwatchPrisma({ name: 'kenny', class: 'support'})
   // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
   // Read all users from the database and print them to the console
-  const allUsers = await prisma.overwatchPrismas()
+  const allUsers = await prisma.owcharacters()
   console.log(allUsers)
 
   // Delete a character
-  const deleteChar = await prisma.deleteOverwatchPrisma({name: 'kenny'})
-  console.log('Character deleted');
+  // const deleteChar = await prisma.deleteOverwatchPrisma({name: 'kenny'})
+  // console.log('Character deleted');
 }
 
 main().catch(e => console.error(e))
